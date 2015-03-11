e493571056f7f1ac01ec00d56f0ff6dd9700ddef
野口真平 <shinpei@noguchimappira-no-MacBook-Air.local>
2015-03-11 21:13:29 +0900

    レビュー後再修正

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,12 +1,11 @@
 require './juice.rb'
 class VendingMachine
 
-
   def initialize
     @sum = 0
     @stocks = {"cola"=> [], "redbull" => []}
     @available_money = [10, 50, 100, 500, 1000, 5000]
-    @paypack = 0
+    @payback = 0
     @sales_amaunt = 0
   end
 
@@ -22,6 +21,8 @@ class VendingMachine
 
   def payback
     puts "#{@payback}円が払い戻されます"
+    @sum = 0
+    @payback = 0
   end
 
   def stock_check?(juice_name)
@@ -38,26 +39,33 @@ class VendingMachine
     }
   end
 
-  def show_stock(juice_name)
-    puts "在庫は#{@stocks[juice_name].count}つあります"
+  def show_stock(juice_name = nil)
+    if juice_name == nil
+      @stocks.keys.count.times {|i|
+        juice_name = @stocks.keys[i]
+        puts "#{juice_name}の在庫は#{@stocks[juice_name].count}つあります"
+      }
+    else
+      puts "#{juice_name}の在庫は#{@stocks[juice_name].count}つあります"
+    end
   end
 
   def show_sales_amaount
     puts "売上は#{@sales_amaunt}円です"
   end
 
-  def seles_management
-    sales = @stocks[juice_name][0].price
+  def sales_management(sales)
     @sales_amaunt += sales
     @sum -= sales
-    @payback = @sum
   end
 
   def buying(juice_name)
     if stock_check?(juice_name) && money_check?(juice_name)
       @stocks[juice_name].pop
+      sales = @stocks[juice_name][0].price
       puts "#{juice_name}を#{sales}円で購入しました！"
-      seles_management
+      sales_management(sales)
+      @payback = @sum
     elsif !stock_check?(juice_name)
       puts "品切れです"
     else
@@ -68,11 +76,11 @@ end
 
 vending_machine = VendingMachine.new
 vending_machine.stock_making("redbull",5)
-vending_machine.show_stock("redbull")
-vending_machine.throwing(200)
+vending_machine.stock_making("cola",5)
+vending_machine.throwing(500)
 vending_machine.buying("redbull")
-vending_machine.buying("redbull")
+vending_machine.show_sales_amaount
 vending_machine.buying("redbull")
 vending_machine.payback
-vending_machine.show_stock("redbull")
+vending_machine.show_stock("cola")
 vending_machine.show_sales_amaount
